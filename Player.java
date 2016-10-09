d5fbefd6f5f17853515393c772ed137b62678bf7
jkjan <jkjan@naver.com>
2016-10-10 02:59:08 +0900

    DiceGame dice Player .java
    Alea iacta est

---
+++ Player.java
@@ -0,0 +1,19 @@
+﻿public class Player {
+private String name=null;
+int num=0;
+public Player(String _name) {
+name=_name;
+}
+public void play() {
+dice d=new dice();
+for(int i=0; i<10;i++)
+d.roll();
+num=d.getNum();
+}
+public String getName() {
+return this.name;
+}
+public int getNum() {
+return this.num;
+}
+}
---
+++ dice.java
@@ -0,0 +1,10 @@
+﻿public class dice {
+private int number = 0;
+public void roll() {
+number = (int) ((Math.random() * 6)+1);
+System.out.println("Turn on " + number);
+}
+public int getNum() {
+return this.number;
+}
+}
