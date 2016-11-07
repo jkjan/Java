cb7b9090a5d2d9552f548abd930eaf8de9d60a07
jkjan <jkjan@naver.com>
2016-11-07 09:32:11 +0900

    j

--- Grade.java
+++ Grade.java
@@ -1,7 +1,8 @@
 class Grade {
 
 public static void WhichGrade(double score) {
-if (score >= 90) {System.out.println("A");}
+if (score > 100) {System.out.println("The score must be lower than 100.");} 
+else if (score >= 90) {System.out.println("A");}
 else if (score >= 80) {System.out.println("B");}
 else if (score >= 70) {System.out.println("C");}
 else if (score >= 60) {System.out.println("D");}
